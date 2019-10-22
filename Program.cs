30e28deb8e6af67c7c21ad082272eb01f04af0cf
Carter Nelson <carternelson05@gmail.com>
2019-10-22 11:21:29 -0700

    Created Card and Deck classes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Card_Deck
 {
+    static class Constants
+    {
+        public static readonly string[] vals = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        public static readonly string[] suits = {"Clubs", "Spades", "Hearts", "Diamonds"};
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Deck dek = new Deck();
+        }
+    }
+    class Card
+    {
+        public string stringVal {get;set;}
+        public string suit {get;set;}
+        public int val {get;set;}
+
+        public Card(string _suit, int _val)
+        {
+            suit = _suit;
+            val = _val;
+            stringVal = Constants.vals[_val-1];
+        }
+    }
+    class Deck
+    {
+        public List<Card> cards = new List<Card>();
+
+        public Deck()
+        {
+            foreach (var suit in Constants.suits)
+            {
+                for (int i = 1; i <= 13; i++)
+                {
+                    cards.Add(new Card(suit, i));
+                }
+            }
         }
     }
 }
